8bc77573f957e4310705d6e6c931b11fbd7ef59c
NoahConder <noah.conder@signalwire.com>
2023-07-28 20:57:40 -0500

    partial commit (dev)

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const axios = require('axios').default;
 
 const app = express();
 
@@ -10,7 +11,8 @@ const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-
+// TODO: Possibly implement a registration system? This system would save SignalWire API information
+//  and OpenWeather API key rather than manually entering it each time when testing.
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -25,15 +27,15 @@ router.get("/", function (req, res) {
   res.render(__dirname + "/views/index.ejs");
 
 });
-// TODO: Implement the Database solution. Should be SQL/SQLite.
+// TODO: Implement the Database solution. Should be SQL/SQLite. (Maybe MongoDB?)
 router.get("/sql", function (req, res) {
     res.render(__dirname + "/views/sql.ejs");
 
 });
-/*
-TODO: Implement API request for pulling call logs from SW API. Ideally I want to pull the following data:
-TODO: Price, Status, Callee, Called, Date & Time
-*/
+
+// TODO: Implement API request for pulling call logs from SW API.
+//  Ideally I'd want to pull the following data: Price, Status, Callee, Called, Date & Time
+
 router.get("/phone_logger", function (req, res) {
     res.render(__dirname + "/views/phone_logger.ejs");
 
@@ -43,7 +45,9 @@ router.get("/weather_lookup", function (req, res) {
     res.render(__dirname + "/views/weather_lookup.ejs");
 
 });
+router.get("/weather_handle", function (req, res) {
 
+});
 // TODO: Add proper error handling. Currently only displays 404 errors. Should also return 500 errors if necessary.
 app.use((req, res, next) => {
     res.status(404).render(__dirname + "/views/error.ejs",{ error_status: '404', error_res: "You've gotten lost! This page does not exist." });
