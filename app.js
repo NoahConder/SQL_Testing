c4b31537c93df67f0be9167540df1f68dfbf1f03
Noah <noah.conder@signalwire.com>
2024-01-29 14:50:12 -0600

    Beautified index.ejs, worked on building dialer(temp), and refined database.

--- app.js
+++ app.js
@@ -1,62 +1,60 @@
+//Imports
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+
+const geocodingHandler = require('./public/handlers/geocoding_handler');
+const phoneHandler = require('./public/handlers/phone_handler');
+const weatherHandler = require('./public/handlers/weather_handler');
+const index = require('./public/routes/index');
+const sql = require('./public/routes/sql');
+const weather = require('./public/routes/weather');
+const calls = require('./public/routes/calls');
+
 const app = express();
-const db = require("./public/javascripts/database")
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+function configureExpress() {
+    app.set('views', path.join(__dirname, 'views'));
+    app.set('view engine', 'ejs');
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+    app.use(logger('dev'));
+    app.use(express.json());
+    app.use(express.urlencoded({extended: false}));
+    app.use(cookieParser());
+    app.use(express.static(path.join(__dirname, 'public')));
+}
 
-const router = express.Router();
+function installRoutes() {
+    app.use('/', index);
+    app.use('/', calls);
+    app.use('/', sql);
+    app.use('/', weather);
+    app.use('/', weatherHandler);
+    app.use('/', phoneHandler);
+    app.use('/', geocodingHandler);
 
-// Import routes
-const geocoding_handler = require('./public/handlers/geocoding_handler')
-const phone_handler = require('./public/handlers/phone_handler')
-const weather_handler = require('./public/handlers/weather_handler')
-const index = require('./public/routes/index')
-const sql = require('./public/routes/sql')
-const weather = require('./public/routes/weather')
-const phone_lookup = require('./public/routes/phone_logger')
-
-app.use('/', index);
-
-app.use('/', sql);
-
-app.use('/', phone_lookup);
-
-app.use('/', weather);
-
-app.use('/', weather_handler);
-
-app.use('/', phone_handler);
-
-app.use('/', geocoding_handler);
-
-// Route for handling 404 errors
-app.use((req, res, next) => {
-    res.status(404).render("error.ejs", {
-        error_status: '404',
-        error_res: "You've gotten lost! This page does not exist."
+    app.use((req, res, next) => {
+        res.status(404).render("error.ejs", {
+            error_status: '404',
+            error_res: "You've gotten lost! This page does not exist."
+        });
     });
-});
 
-// Route for handling 500 errors
-app.use((err, req, res, next) => {
-    console.error(err); // Log the error for debugging purposes
-    res.status(500).render("error.ejs", {
-        error_status: '500',
-        error_res: "Something went wrong. Please try again later."
+    app.use((err, req, res, next) => {
+        console.error(err);
+        res.status(500).render("error.ejs", {
+            error_status: '500',
+            error_res: "Something went wrong. Please try again later."
+        });
     });
-});
+}
 
-app.listen(8080, host="0.0.0.0");
+function startServer() {
+    app.listen(8080, "0.0.0.0");
+    console.log("http://127.0.0.1:8080");
+}
 
-console.log("http://127.0.0.1:8080");
+configureExpress();
+installRoutes();
+startServer();
